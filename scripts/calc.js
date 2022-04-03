9c61c82a2acf24ab5816c83023885d31f0b93574
Veronica Lourens <Veronica@lourens.cloud>
2022-04-03 05:45:23 +0000

    Add index.html file.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,9 @@
-function addition() {
-    return 42;
+// function addition() {
+//     return 42;
+// }
+
+function addition(num1, num2) {
+    return num1 + num2;
 }
 
 
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,11 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
 const { test, expect } = require('@jest/globals');
 const addition = require('../calc');
 
 describe('calculator', () => {
+
     describe('Addition function', () => {
+
         test('Should return 42 for 20 + 22', () => {
             expect(addition(20, 22)).toBe(42);
-        })
+        });
+
+        test('Should return 73 for 42 + 31', () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+
+        // test("Should return 550 for 5 + '50' ", () => {
+        //     expect(addition(5, '50')).toBe(550);
+        // });
+
+
 
     });
 
